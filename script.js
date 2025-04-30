000320f01aef1e439566e4a87a1498d76e767e3d
Nikhil Shrivastava <nikhil.shrivastava304@gmail.com>
2025-04-30 19:14:01 +0530

    Implement Light/Dark Mode toggle functionality

---
+++ script.js
@@ -0,0 +1,21 @@
+const themeToggle = document.querySelector(".theme-toggle");
+
+// Set theme based on saved preference or system default
+(() => {
+    const savedTheme = localStorage.getItem("theme");
+    const systemPrefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    const isDarkTheme = savedTheme === "dark" || (!savedTheme && systemPrefersDark);
+    document.body.classList.toggle("dark-theme", isDarkTheme);
+    themeToggle.querySelector("i").className = isDarkTheme ? "fa-solid fa-sun" : "fa-solid fa-moon";
+})();
+
+
+//Switch between light and dark themes
+const toggleTheme = () => {
+    const isDarkTheme = document.body.classList.toggle("dark-theme");
+    localStorage.setItem("theme", isDarkTheme ? "dark" : "light");
+    themeToggle.querySelector("i").className = isDarkTheme ? "fa-solid fa-sun" : "fa-solid fa-moon";
+}
+
+themeToggle.addEventListener("click", toggleTheme);
